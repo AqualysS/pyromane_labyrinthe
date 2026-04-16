9d3fef9a17d1ad612e6a222b6ad9efb511c92304
Xeroy456 <chiodoquentin03@gmail.com>
2026-04-16 11:10:31 +0200

    Add first person

--- pyro_laby.py
+++ pyro_laby.py
@@ -1,1 +1,18 @@
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+
+ground = Entity(
+    model='plane',
+    scale=(100,1,100),
+    texture='white_cube',
+    texture_scale=(100,100),
+    collider='box')
+
+player = FirstPersonController()
+
+DirectionalLight()
+AmbientLight()
+
+app.run()
